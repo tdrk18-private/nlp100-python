c9b1d93b3c156c8061ae23b18c2968ffc54d959d
todoroki <s18.todoroki+git@gmail.com>
2020-04-19 23:20:09 +0900

    add count strings of text ignore string that is not alphabets

--- Section1/reverse.py
+++ Section1/reverse.py
@@ -1,3 +1,7 @@
+import re
+from typing import List
+
+
 class Section1:
     @staticmethod
     def reverse(s: str) -> str:
@@ -13,4 +17,13 @@ class Section1:
         for a, b in zip(list(s), list(t)):
             res += a
             res += b
+        return res
+
+    @staticmethod
+    def pi_list(s: str) -> List[int]:
+        res = []
+        s_list = s.split(" ")
+        for text in s_list:
+            clean_text = re.sub(r"\W", "", text)
+            res.append(len(clean_text))
         return res
--- tests/Section1/test_reverse.py
+++ tests/Section1/test_reverse.py
@@ -14,3 +14,10 @@ def test_extract_odd():
 def test_merge_string():
     result = Section1.merge_strings("パトカー", "タクシー")
     assert result == "パタトクカシーー"
+
+
+def test_pi_list():
+    text = "Now I need a drink, alcoholic of course, " \
+           "after the heavy lectures involving quantum mechanics."
+    result = Section1.pi_list(text)
+    assert result == [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
